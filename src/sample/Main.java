694918947a4860d6f574eb681c31d42a2cc889bf
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2018-12-11 13:12:15 +0100

    Project setting update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-//    @Override
+    @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
